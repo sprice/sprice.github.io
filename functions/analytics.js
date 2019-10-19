9975c57c24645207f8dc5a5cb3a4ec1dbd0d85f8
Shawn Price <shawnp@gmail.com>
2019-10-19 08:56:07 -0700

    Better handling of analytics;

--- functions/analytics.js
+++ functions/analytics.js
@@ -1,24 +1,31 @@
 const cookie = require('cookie')
 const ua = require('universal-analytics')
 
+const now = new Date().toISOString()
 const returnBody = { statusCode: 200, body: "" }
+const baseDomain = process.env.BASE_DOMAIN
+
+const log = (msg, value) => {
+  console.log(`${now}: ${msg} :: ${value}`)
+}
 
 exports.handler = function(event, context, callback) {
 
   if (process.env.NETLIFY_DEV) {
+    log('Exiting: will not run on dev environment')
     return callback(null, returnBody)
   }
-
-  let baseDomain = process.env.BASE_DOMAIN
 
   if (!event.headers.cookie) {
+    log('Exiting: No cookies received')
     return callback(null, returnBody)
   }
 
-  console.log('here is the cookie', event.headers.cookie)
-  console.log('typeof cookie', typeof event.headers.cookie)
+  log('here is the cookie', event.headers.cookie)
+  log('typeof cookie', typeof event.headers.cookie)
 
   if (typeof event.headers.cookie != "string") {
+    log('Exiting: cookies are not a string')
     return callback(null, returnBody)
   }
 
@@ -32,6 +39,7 @@ exports.handler = function(event, context, callback) {
   var visitor = ua(process.env.GOOGLE_ANALYTICS_ID, uuid)
   const page = event.headers.referer.split(baseDomain)[1]
 
+  log('Pageview tracked', uuid)
   visitor.pageview(page).send()
 
   return callback(null, returnBody);
