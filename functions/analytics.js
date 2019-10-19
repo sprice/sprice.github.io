f3b451e0dcb86fea64e0dec4d53d6774925febc6
Shawn Price <shawnp@gmail.com>
2019-10-18 21:10:21 -0700

    Better GA handling

--- functions/analytics.js
+++ functions/analytics.js
@@ -9,16 +9,23 @@ exports.handler = function(event, context, callback) {
 
   let baseDomain = process.env.BASE_DOMAIN
 
-  console.log('cookie', event.headers.cookie)
+  if (!event.headers.cookie) {
+    callback(null)
+  }
 
   const cookies = cookie.parse(event.headers.cookie)
   const uuid = cookies.uuid
+
+  if (!uuid) {
+    callback(null)
+  }
+
   var visitor = ua(process.env.GOOGLE_ANALYTICS_ID, uuid)
   const page = event.headers.referer.split(baseDomain)[1]
 
   visitor.pageview(page).send()
 
-  callback(null, {
+callback(null, {
   statusCode: 200,
   body: ""
   });
